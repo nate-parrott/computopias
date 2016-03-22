f7b940c80c7b7c03de16481a447de5614f854d47
= <nateparro2t@gmail.com>
2016-03-22 14:06:12 -0400

    profile view

--- Computopias/Data.swift
+++ Computopias/Data.swift
@@ -21,3 +21,30 @@ typealias Poster = Firebase
 
 
 typealias GridSize = CGSize
+
+struct Data {
+    static func getUID() -> String {
+        let key = "UID"
+        if (NSUserDefaults.standardUserDefaults().valueForKey(key) as? String) == nil {
+            NSUserDefaults.standardUserDefaults().setValue(NSUUID().UUIDString, forKey: key)
+        }
+        return NSUserDefaults.standardUserDefaults().valueForKey(key) as! String
+    }
+    static func getName() -> String? {
+        return NSUserDefaults.standardUserDefaults().valueForKey("ProfileName") as? String
+    }
+    static func setName(name: String) {
+        // TODO: set name in firebase
+        NSUserDefaults.standardUserDefaults().setValue(name, forKey: "ProfileName")
+    }
+    static func getBio() -> String? {
+        return NSUserDefaults.standardUserDefaults().valueForKey("ProfileBio") as? String
+    }
+    static func setBio(bio: String) {
+        // TODO: set bio in firebase
+        NSUserDefaults.standardUserDefaults().setValue(bio, forKey: "ProfileBio")
+    }
+    static func profileJson() -> [String: AnyObject] {
+        return ["name": getName() ?? "", "bio": getBio() ?? "", "uid": getUID()]
+    }
+}
